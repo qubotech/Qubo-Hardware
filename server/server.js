8b4e67dcc06810f0a8994d8921863206e2b5eeb2
Mr. AKS <47912137+AKSKumar2002@users.noreply.github.com>
2025-11-13 15:04:59 +0530

    3

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectCloudinary();
 // ✅ Only use CORS once — this is the correct one
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://farmpickshope.vercel.app'
+  'https://qubo-hardware.vercel.app/'
 ];
 
 app.use(cors({
