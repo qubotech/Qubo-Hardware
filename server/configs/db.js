533de484ee9399a141120244cfa7b6f9bc633180
Mr. AKS <47912137+AKSKumar2002@users.noreply.github.com>
2025-11-14 11:26:01 +0530

    38

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected', ()=> console.log("Database Connected")
         );
-        await mongoose.connect(`${process.env.MONGODB_URI}/greencart`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/qubo`)
         console.log("MongoDB connection successful");
     } catch (error) {
         console.error("Database connection error:", error.message);
